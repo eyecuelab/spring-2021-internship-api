cab1b40102ecd040a69794d218703e57eaedb45e
Aaron Ross <aaron.ross@eyecuelab.com>
2021-04-21 11:36:52 -0700

    add users service

--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -1,94 +1,38 @@
-import { Request, Response, Router } from 'express';
-import { BAD_REQUEST, CREATED, OK, NOT_FOUND } from 'http-status-codes';
-import { ParamsDictionary } from 'express-serve-static-core';
-import { getConnection } from 'typeorm';
-import { validate } from 'class-validator';
-import { User } from '../entities/User';
-import { paramMissingError } from '../shared/constants';
+import { Router } from 'express';
+import UsersService from '../services/Users';
 
 // Init shared
 const router = Router();
 
 /******************************************************************************
- *                      Get All Users - "GET /api/users/all"
+ *                      Get All Users - "GET /api/users"
  ******************************************************************************/
 
-router.get('/all', async (req: Request, res: Response) => {
-  const users = await getConnection().getRepository(User).find();
-  return res.status(OK).json({ users });
-});
+router.get('/', UsersService.list);
 
 /******************************************************************************
  *                      Get User - "GET /api/users/:id"
  ******************************************************************************/
 
-router.get('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params as ParamsDictionary;
-  const user = await getConnection().getRepository(User).findOne(id);
-  if (!user) {
-    res.status(NOT_FOUND);
-    res.end();
-    return;
-  }
-  return res.status(OK).json({ user });
-});
+router.get('/:id', UsersService.one);
 
 /******************************************************************************
- *                       Add One - "POST /api/users/add"
+ *                       Add One - "POST /api/users"
  ******************************************************************************/
 
-router.post('/add', async (req: Request, res: Response) => {
-  const { user: input } = req.body;
-  const user = new User();
-  user.firstName = input.firstName;
-  user.lastName = input.lastName;
-  user.email = input.email;
-  user.age = input.age;
-  const valid = await validate(user);
-  // TODO: check validation and return error message
-  if (!user) {
-    // UPDATE TO MATCH :ID
-    return res.status(BAD_REQUEST).json({
-      error: paramMissingError,
-    });
-  }
-  // add validation
-  const data = await getConnection().getRepository(User).save(user);
-  return res.status(CREATED).json({ user: data });
-});
+router.post('/', UsersService.add);
 
 /******************************************************************************
- *                       Update - "PUT /api/users/update"
+ *                       Update - "PUT /api/users"
  ******************************************************************************/
 
-router.put('/update', async (req: Request, res: Response) => {
-  const { user } = req.body;
-  if (!user && !user.id) {
-    // UPDATE TO MATCH :ID
-    return res.status(BAD_REQUEST).json({
-      error: paramMissingError,
-    });
-  }
-  // add validation and only set provided fields
-  const data = await getConnection().getRepository(User).save(user);
-  return res.status(OK).json({ user: data });
-});
+router.put('/:id', UsersService.update);
 
 /******************************************************************************
- *                    Delete - "DELETE /api/users/delete/:id"
+ *                    Delete - "DELETE /api/users/:id"
  ******************************************************************************/
 
-router.delete('/delete/:id', async (req: Request, res: Response) => {
-  const repository = await getConnection().getRepository(User);
-  const { id } = req.params as ParamsDictionary;
-  const user = await repository.findOne(id);
-  if (!user) {
-    // UPDATE TO MATCH :ID
-    return res.status(BAD_REQUEST);
-  }
-  await repository.remove([user]);
-  return res.status(OK).end();
-});
+router.delete('/:id', UsersService.remove);
 
 /******************************************************************************
  *                                     Export
