b019700c71ae18644141558db77763431ff97bda
Aaron Ross <aaron.ross@eyecuelab.com>
2021-04-20 18:20:09 -0700

    fix error response add todo for validatio n and other error responses

--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -1,7 +1,8 @@
 import { Request, Response, Router } from 'express';
-import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
+import { BAD_REQUEST, CREATED, OK, NOT_FOUND } from 'http-status-codes';
 import { ParamsDictionary } from 'express-serve-static-core';
 import { getConnection } from 'typeorm';
+import { validate } from 'class-validator';
 import { User } from '../entities/User';
 import { paramMissingError } from '../shared/constants';
 
@@ -25,7 +26,9 @@ router.get('/:id', async (req: Request, res: Response) => {
   const { id } = req.params as ParamsDictionary;
   const user = await getConnection().getRepository(User).findOne(id);
   if (!user) {
-    return res.status(BAD_REQUEST);
+    res.status(NOT_FOUND);
+    res.end();
+    return;
   }
   return res.status(OK).json({ user });
 });
@@ -35,16 +38,23 @@ router.get('/:id', async (req: Request, res: Response) => {
  ******************************************************************************/
 
 router.post('/add', async (req: Request, res: Response) => {
-  const { user } = req.body;
-
+  const { user: input } = req.body;
+  const user = new User();
+  user.firstName = input.firstName;
+  user.lastName = input.lastName;
+  user.email = input.email;
+  user.age = input.age;
+  const valid = await validate(user);
+  // TODO: check validation and return error message
   if (!user) {
+    // UPDATE TO MATCH :ID
     return res.status(BAD_REQUEST).json({
       error: paramMissingError,
     });
   }
   // add validation
-  await getConnection().getRepository(User).save(user);
-  return res.status(CREATED).json({ user });
+  const data = await getConnection().getRepository(User).save(user);
+  return res.status(CREATED).json({ user: data });
 });
 
 /******************************************************************************
@@ -54,6 +64,7 @@ router.post('/add', async (req: Request, res: Response) => {
 router.put('/update', async (req: Request, res: Response) => {
   const { user } = req.body;
   if (!user && !user.id) {
+    // UPDATE TO MATCH :ID
     return res.status(BAD_REQUEST).json({
       error: paramMissingError,
     });
@@ -72,6 +83,7 @@ router.delete('/delete/:id', async (req: Request, res: Response) => {
   const { id } = req.params as ParamsDictionary;
   const user = await repository.findOne(id);
   if (!user) {
+    // UPDATE TO MATCH :ID
     return res.status(BAD_REQUEST);
   }
   await repository.remove([user]);
