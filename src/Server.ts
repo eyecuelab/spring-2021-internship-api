f86177f78f908ea0afa32dc2b895a211ad70f773
Zahnen Garner <zahnen@gmail.com>
2021-04-30 21:27:35 -0700

    add express-session, user assignment middleware

--- src/Server.ts
+++ src/Server.ts
@@ -1,17 +1,36 @@
-import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import path from "path";
 import helmet from "helmet";
+import session from "express-session";
 
 import express, { Request, Response, NextFunction } from "express";
+import { getConnection } from "typeorm";
+import { User } from "./entities/User";
 import { BAD_REQUEST } from "http-status-codes";
 import cors from "cors";
 
 import BaseRouter from "./routes";
 import logger from "./shared/Logger";
 
+declare module "express-session" {
+  export interface SessionData {
+    userId: string;
+  }
+}
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: User;
+  }
+}
+
 // Init express
 const app = express();
+//Init Session
+const sess = {
+  userId: 0,
+  cookie: {},
+  secret: "This is super secret",
+};
 
 /************************************************************************************
  *                              Set basic express settings
@@ -20,7 +39,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(session(sess));
+app.use(async (req, res, next) => {
+  const user = await getConnection()
+    .getRepository(User)
+    .findOne(req.session.userId);
+  req.user = user;
+  next();
+});
 
 // Show routes called in console during development
 if (process.env.NODE_ENV === "development") {
