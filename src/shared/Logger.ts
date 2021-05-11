cd4d2ca35fb74e6bbddd1c604f9e8d842dea709a
Aaron Ross <aaron.ross@eyecuelab.com>
2021-05-11 16:43:18 -0700

    update logging

--- src/shared/Logger.ts
+++ src/shared/Logger.ts
@@ -32,6 +32,18 @@ if (process.env.NODE_ENV === 'production') {
   });
   logger.add(errTransport);
   logger.add(infoTransport);
+  const errorStackFormat = format((info) => {
+    if (info.stack) {
+      // tslint:disable-next-line:no-console
+      console.log(info.stack);
+      return false;
+    }
+    return info;
+  });
+  const consoleTransport = new Console({
+    format: format.combine(format.colorize(), format.simple(), errorStackFormat()),
+  });
+  logger.add(consoleTransport);
 } else {
   const errorStackFormat = format((info) => {
     if (info.stack) {
