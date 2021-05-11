1762ce9cb4a6601ed1352cb40f4025142872602d
Aaron Ross <aaron.ross@eyecuelab.com>
2021-05-11 16:54:03 -0700

    update cookie settings

--- src/Server.ts
+++ src/Server.ts
@@ -32,7 +32,9 @@ const app = express();
 /************************************************************************************
  *                              Set basic express settings
  ***********************************************************************************/
-
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1);
+}
 app.use(
   cors({
     origin: process.env.CLIENT_URL ?? 'self',
