fdc4f83a13b9c9c3fc95cef6b79076aef0f2c7d0
Aaron Ross <aaron.ross@eyecuelab.com>
2021-05-13 16:07:52 -0700

    update cli

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,9 +1,9 @@
-import { ConnectionOptions } from "typeorm";
+import { ConnectionOptions } from 'typeorm';
 
-const env = process.env.NODE_ENV ?? "development";
-if (env !== "production") {
+const env = process.env.NODE_ENV ?? 'development';
+if (env !== 'production') {
   // eslint-disable-next-line
-  const dotenv = require("dotenv");
+  const dotenv = require('dotenv');
   const envVars = dotenv.config({
     path: `./env/${env}.env`,
   });
@@ -12,30 +12,24 @@ if (env !== "production") {
   }
 }
 
-const ext = env === "production" ? "js" : "ts";
+const ext = env === 'production' ? 'js' : 'ts';
 
 export const config: ConnectionOptions = {
-  type: "postgres",
+  type: 'postgres',
   host: process.env.TYPEORM_HOST,
-  url:
-    process.env.NODE_ENV === "production"
-      ? process.env.DATABASE_URL
-      : undefined,
-  port: parseInt(process.env.TYPEORM_PORT ?? "5000"),
+  url: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : undefined,
+  port: parseInt(process.env.TYPEORM_PORT ?? '5000'),
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  synchronize: process.env.NODE_ENV === "test",
+  synchronize: process.env.NODE_ENV === 'test',
   entities: [__dirname + `/entities/*.${ext}`],
   migrations: [__dirname + `/migration/*.${ext}`],
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   cli: {
     entitiesDir: __dirname + `/entities`,
     migrationsDir: __dirname + `/migration`,
     subscribersDir: __dirname + `/subscriber`,
   },
-  logging: process.env.DEBUG === "typeorm",
+  logging: process.env.DEBUG === 'typeorm',
 };
